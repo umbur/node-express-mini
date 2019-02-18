c7ed43b9a81f57c343b1c92f42ab637bb470fe20
umbur <work1415@gmail.com>
2019-02-18 14:58:57 -0700

    created POST request with the corresponding error messages

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // implement your API here
+
 const express = require("express");
 
 const db = require("./data/db");
 
 const server = express();
+
+// middleware
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("<h2>Hello From Local Host</h2>");
@@ -15,8 +19,55 @@ server.get("/api/users", (req, res) => {
     .then(users => {
       res.status(201).json({ success: true, users });
     })
-    .catch(({ code, message }) => {
-      res.status(code).json({ success: false, message });
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "There was an error while saving the user to the database"
+      });
+      res.end();
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  
+
+  db.findById(userId)
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ error: "The user with the specified ID does not exist" });
+      } else {
+        res.status(201).json({ success: true, user })
+      }
+      
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, error: "The user information could not be retrieved." })
+    });
+});
+
+// POST
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  const newUser = req.body;
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+
+  db.insert(newUser)
+    .then(user => {
+      res.status(201).json({ success: true, user });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "There was an error while saving the user to the database"
+      });
     });
 });
 
